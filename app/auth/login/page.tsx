649820494f7b32a28422f5340d96d771a40bb4f9
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2024-02-04 23:02:32 +0530

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,6 @@
 import { Metadata } from "next";
 import { LoginForm } from "@/components/auth/login-form";
 
-export const metadata: Metadata = {
-  title: "Login",
-};
 
 const LoginPage = () => {
   return <LoginForm />;
