f5e8f8e851c2e12c4405e3f116d6c7aefa9b4938
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2024-02-07 19:21:37 +0530

    fixed setting page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,25 +1,10 @@
-"use client";
 import { Metadata } from "next";
-import { logout } from "@/actions/logout";
-import { useCurrentUser } from "@/hooks/use-current-user";
+import Settings from "../_components/setting";
 
 export const metadata: Metadata = {
   title: "Settings",
 };
 
-const SettingsPage = () => {
-  const user = useCurrentUser();
-  const onClick = () => {
-    logout();
-  };
-
-  return (
-    <div className="bg-white p-10 rounded-xl">
-      <button onClick={onClick} type="submit">
-        Sign out
-      </button>
-    </div>
-  );
-};
-
-export default SettingsPage;
+export default function SettingsPage() {
+  <Settings />;
+}
